1a33d05ff3cb13ec06e8ffef5d2e9225da7bbbbf
Muhammad Rivaldhi <mhmmdrivaldhi10@gmail.com>
2026-05-11 00:10:28 +0700

    Feat:created authentication feature using Auth js and integrate login with google account

--- components/shared/login-button.tsx
+++ components/shared/login-button.tsx
@@ -1,10 +1,16 @@
+import { signIn } from "@/auth";
 import { FaG } from "react-icons/fa6"
 
 export const LoginGoogleButton = () => {
     return (
-        <button className="flex items-center justify-center gap-2 w-full bg-blue-700 hover:bg-blue-800 text-white font-semibold transition duration-400 py-3 px-6 text-base rounded-lg cursor-pointer">
-            <FaG size={16}/>
-            Sign In With Google Account
-        </button>
+        <form action={async () =>  {
+            "use server";
+            await signIn("google");
+        }}>    
+            <button className="flex items-center justify-center gap-2 w-full bg-blue-700 hover:bg-blue-800 text-white font-semibold transition duration-400 py-3 px-6 text-base rounded-lg cursor-pointer">
+                <FaG size={16}/>
+                Sign In With Google Account
+            </button>
+        </form>
     )
 }
