3cd4ceae6f9b9b65019960fb7a6d5f2a3e30d60f
Muhammad Rivaldhi <mhmmdrivaldhi10@gmail.com>
2026-05-18 02:37:56 +0700

    Feat: creating feature for delete & update room data

---
+++ types/room.ts
@@ -0,0 +1,9 @@
+import { Prisma } from "@prisma/client";
+
+export type RoomProps = Prisma.RoomGetPayload<{
+    include: {
+        RoomAmenities: {
+            select: {amenitiesId: true}
+        }
+    }
+}>
